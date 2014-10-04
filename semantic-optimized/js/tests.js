67713e695f959b619f8c495a9632dc0c99f74a99
jessec <robertjessecooke@gmail.com>
2014-10-05 01:54:18 +0200

    cc

--- semantic-optimized/js/tests.js
+++ semantic-optimized/js/tests.js
@@ -2,25 +2,45 @@
  * start test if css animations are available
  */
 var elm = document.createElement("detect");
-var animation = false,
-    animationstring = 'animation',
-    keyframeprefix = '',
-    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-    pfx  = '';
+var animation = false, animationstring = 'animation', keyframeprefix = '', domPrefixes = 'Webkit Moz O ms Khtml'
+		.split(' '), pfx = '';
 
-if( elm.style.animationName !== undefined ) { animation = true; }
+if (elm.style.animationName !== undefined) {
+	animation = true;
+}
 
-if( animation === false ) {
-  for( var i = 0; i < domPrefixes.length; i++ ) {
-    if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
-      pfx = domPrefixes[ i ];
-      animationstring = pfx + 'Animation';
-      keyframeprefix = '-' + pfx.toLowerCase() + '-';
-      animation = true;
-      break;
-    }
-  }
+if (animation === false) {
+	for (var i = 0; i < domPrefixes.length; i++) {
+		if (elm.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
+			pfx = domPrefixes[i];
+			animationstring = pfx + 'Animation';
+			keyframeprefix = '-' + pfx.toLowerCase() + '-';
+			animation = true;
+			break;
+		}
+	}
 }
 /*
  * end test if css animations are available
+ */
+
+/*
+ * start test if css rem are available
+ */
+
+(function() {
+	var element, size
+	element = document.createElement('p')
+	element.innerHTML = 'How now brown cow?'
+	element.style.cssText = 'font-size: 1.5rem'
+	size = element.style.fontSize
+	if (size.indexOf('rem') < 0) {
+		// ai no rem support
+	} else {
+		// yup rem support
+	}
+})()
+
+/*
+ * end test if css rem are available
  */
