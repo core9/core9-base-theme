d58b63cac1a691aec4dc8f27220b6df0ec08cdd3
jessec <robertjessecooke@gmail.com>
2014-10-06 19:04:57 +0200

    cc

--- json-editor/examples/js/wizard-engine.js
+++ json-editor/examples/js/wizard-engine.js
@@ -1,36 +1,50 @@
-		function getStep(step, label) {
-			var li = document.createElement("li");
-			var a = document.createElement("a");
-			a.setAttribute("href", "#");
-			a.appendChild(document.createTextNode(label));
-			li.appendChild(a);
-			var div = document.createElement("div");
-			div.setAttribute("class", "main-container form-container");
-			var innerDiv = document.createElement("div");
-			innerDiv.setAttribute("id", "editor_holder-" + step);
-			div.appendChild(innerDiv);
-			var button = document.createElement("button");
-			button.setAttribute("id", "submit-" + step);
-			button.appendChild(document.createTextNode("Submit"));
-			div.appendChild(button);
-			li.appendChild(div);
-			return li;
-		}
+var Wizard = {
 
-		function getScript(script, step) {
-			$LAB.script(script).wait(function() {
-				init(step);
-			});
-		}
+	getStep : function(step, label) {
+		var li = document.createElement("li");
+		var a = document.createElement("a");
+		a.setAttribute("href", "#");
+		a.appendChild(document.createTextNode(label));
+		li.appendChild(a);
+		var div = document.createElement("div");
+		div.setAttribute("class", "main-container form-container");
+		var innerDiv = document.createElement("div");
+		innerDiv.setAttribute("id", "editor_holder-" + step);
+		div.appendChild(innerDiv);
+		var button = document.createElement("button");
+		button.setAttribute("id", "submit-" + step);
+		button.appendChild(document.createTextNode("Submit"));
+		div.appendChild(button);
+		li.appendChild(div);
+		return li;
+	},
 
-		function createWizard(steps) {
-			var ul = document.getElementById('tab-ul');
-			for ( var step in steps) {
-				var script = steps[step].file;
-				ul.appendChild(getStep(step, steps[step].label));
-				getScript(script, step);
+	getScript : function(script, step) {
+		$LAB.script(script).wait(function() {
+			init(step);
+		});
+	},
+
+	createWizard : function(steps) {
+		var ul = document.getElementById('tab-ul');
+		for ( var step in steps) {
+			var script = steps[step].file;
+			ul.appendChild(Wizard.getStep(step, steps[step].label));
+			Wizard.getScript(script, step);
+		}
+	},
+
+	hideAllDivs : function() {
+		var divs = document.getElementsByClassName('main-container');
+		for ( var div in divs) {
+			var elem = divs[div];
+			if (elem.tagName == 'DIV') {
+				elem.style.display = "none";
 			}
 		}
+	},
+
+	init : function() {
 
 		promise.get('wizards/basic/steps.json').then(
 				function(error, text, xhr) {
@@ -38,18 +52,8 @@
 						alert('Error ' + xhr.status);
 						return;
 					}
-					createWizard(JSON.parse(text))
+					Wizard.createWizard(JSON.parse(text))
 				});
-
-		function hideAllDivs() {
-			var divs = document.getElementsByClassName('main-container');
-			for ( var div in divs) {
-				var elem = divs[div];
-				if (elem.tagName == 'DIV') {
-					elem.style.display = "none";
-				}
-			}
-		}
 
 		document.getElementById('tab-ul')
 				.addEventListener(
@@ -58,10 +62,16 @@
 							if (e.target.tagName != 'A') {
 								return;
 							}
-							hideAllDivs();
+							Wizard.hideAllDivs();
 							var elem = e.target.parentNode
 									.getElementsByTagName('div')[0];
 							if (elem !== undefined && elem.tagName == 'DIV') {
 								elem.style.display = "block";
 							}
 						});
+
+	}
+
+}
+
+Wizard.init();
