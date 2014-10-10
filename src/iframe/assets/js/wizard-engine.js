20012ad7bd5d3c0d8af23172cae9a2b9f479bdf4
jessec <robertjessecooke@gmail.com>
2014-10-10 16:33:14 +0200

    cc

--- src/iframe/assets/js/wizard-engine.js
+++ src/iframe/assets/js/wizard-engine.js
@@ -2,6 +2,7 @@ var Wizard = {
 	json : {},
 	getStep : function(step, label) {
 		var li = document.createElement("li");
+		li.setAttribute("class", "step");
 		var a = document.createElement("a");
 		a.setAttribute("href", "#");
 		a.appendChild(document.createTextNode(label));
@@ -48,6 +49,27 @@ var Wizard = {
 				elem.style.display = "none";
 			}
 		}
+	},
+
+	getParentElementWithClass : function(element, className){
+		function collectionHas(a, b) { //helper function (see below)
+		    for(var i = 0, len = a.length; i < len; i ++) {
+		        if(a[i] == b) return true;
+		    }
+		    return false;
+		}
+		function findParentBySelector(elm, selector) {
+		    var all = document.querySelectorAll(selector);
+		    var cur = elm.parentNode;
+		    while(cur && !collectionHas(all, cur)) { //keep going up until you find a match
+		        cur = cur.parentNode; //go up
+		    }
+		    return cur; //will return null if not found
+		}
+
+
+		var parent = findParentBySelector(element, "." + className);
+		return parent;
 	},
 
 	endsWith : function(str, suffix) {
--- src/iframe/wizards/video/step-1.js
+++ src/iframe/wizards/video/step-1.js
@@ -1,66 +1,53 @@
 function init(step){
 
-
-promise.get('wizards/video/basic.json').then(
+promise.get('wizards/video/video.json').then(
 				function(error, text, xhr) {
 					if (error) {
 						alert('Error ' + xhr.status);
 						return;
 					}
-
 					var starting_value = [ text ];
-
 					// Initialize the editor
 					var editor = new JSONEditor(document
 							.getElementById('editor_holder-' + step), {
 						// Enable fetching schemas via ajax
 						ajax : true,
-
 						// The schema for the editor
 						schema : {
 							type : "object",
 							title : "Video",
 							  "properties": {
-							    "first_name": {
-							      "type": "string"
-							    },
-							    "last_name": {
-							      "type": "string"
-							    },
-							    "full_name": {
+							    "url": {
 							      "type": "string",
-							      "template": "{{fname}} {{lname}}",
-							      "watch": {
-							        "fname": "first_name",
-							        "lname": "last_name"
-							      }
-							    }
+							      "default": "http://youtu.be/kDfw4yt554g",
+							    },
+							    "height": {
+							      "type": "string",
+							      "default": "315",
+							    },
+							    "width": {
+								      "type": "string",
+								      "default": "560",
+								},
 							  }
 						},
-
 						// Seed the form with a starting value
 						startval : starting_value,
-
 						// Disable additional properties
 						no_additional_properties : true,
-
 						disable_edit_json : true,
-
 						disable_properties : true,
-
 						disable_collapse : true,
-
 						// Require all properties by default
 						required_by_default : true
 					});
-
-
-
 				      // Hook up the submit button to log to the console
 				      document.getElementById('submit-' + step).addEventListener('click',function(e) {
 				    	  e.stopPropagation();
 				    	  console.log(editor.validate());
-				        // Get the value from the editor
+				    	  var selectedLi = Wizard.getParentElementWithClass(e.target, 'step');
+				    	  Wizard.hideAllDivs();
+				    	  selectedLi.nextSibling.querySelector('.main-container').style.display = "block";
 				    	  console.log('sending to swagger api');
 				        console.log(editor.getValue());
 				      });
